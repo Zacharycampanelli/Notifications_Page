8998115f921623f7e42a3506c61dd108a8f2dcea
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-06-18 22:36:25 -0400

    added data file to seed app

---
+++ src/assets/data/notifications.js
@@ -0,0 +1,52 @@
+export const notifications = [{
+    type: 'reaction',
+    data: {
+      user: 'Mark Webber',
+      post: 'My first tournament today!',
+      time: '1m'
+    }
+  },{
+    type: 'follow',
+    data: {
+      user: 'Angela Gray',
+      time: '5m'
+    }
+  },{
+    type: 'group',
+    data: {
+      user: 'Jacob Thompson',
+      groupName: 'Chess Club',
+      action: 'joined',
+      time: '1d'
+    }
+  },{
+    type: 'message',
+    data: {
+      user: 'Rizky Hasanuddin',
+      message: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+      time: '5d'
+    }
+  },{
+    type: 'comment',
+    data: {
+      user: 'Kimberly Smith',
+      post: 'picture',
+      time: '1w'
+    }
+  }, {
+    type: 'reaction',
+    data: {
+      user: 'Nathan Peterson',
+      post: '5 end-game strategies to increase your win rate',
+      time: '2w'
+    }
+  }, {
+    type: 'group',
+    data: {
+      user: 'Anna Kim',
+      groupName: 'Chess Club',
+      action: 'left',
+      time: '2w'
+    }
+  }]
+
